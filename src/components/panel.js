f508f0f381cc164d6bc9d5caaa704d1aac022aa8
douglaselee <douglaselee@comcast.net>
2017-05-25 12:12:30 -0400

    Stop clearing data from conditionally hidden layout components

--- src/components/panel.js
+++ src/components/panel.js
@@ -8,6 +8,7 @@ module.exports = function(app) {
         template: 'formio/components/panel.html',
         group: 'layout',
         settings: {
+          clearOnHide: false,
           key: 'panel',
           input: false,
           title: '',
